bc5a19c9e57971979f9324503e230c22b940a12f
Jonnester <84779222+jon123boss@users.noreply.github.com>
2024-07-11 20:32:53 +0800

    hello

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-import numpy as np
-# Function to interact with the environment (slot machines)
-def play_slot_machine(action):
-    probabilities = [0.1, 0.9, 0.5, 0.2]
-    if np.random.uniform(0,1) <= probabilities[action]:
-        return 100
-    else:
-        return 0
+poop
